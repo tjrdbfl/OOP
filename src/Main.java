595b96300672140feddd158fc67b672f4fdd6150
tjrdbfl <tjrdbfl123@gmail.com>
2024-02-26 18:00:27 +0900

    feat(javax swing)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
+import view.BoardView;
+import view.JoinView;
+import view.KaupView;
+import view.SubjectView;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true) {
+            System.out.println("0-종료 1-회원가입 2-성적표 3-게시판 4-카우프");
+
+            String control=sc.next();
+
+            switch (control){
+                case "0": return;
+                case "1": JoinView.main(sc);break;
+                case "2": SubjectView.main();break;
+                case "3": BoardView.main();break;
+                case "4": KaupView.main(sc);break;
+            }
         }
     }
 }
